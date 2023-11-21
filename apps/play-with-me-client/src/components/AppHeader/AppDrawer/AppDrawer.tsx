bf4fb70f85b39ea5f4f8eadc4e91774c739db59d
stav1236 <stavmaor1@gmail.com>
2023-11-21 14:10:43 +0200

    finish the structure of the app drawer

--- apps/play-with-me-client/src/components/AppHeader/AppDrawer/AppDrawer.tsx
+++ apps/play-with-me-client/src/components/AppHeader/AppDrawer/AppDrawer.tsx
@@ -1,7 +1,10 @@
 import { useDarkMode } from '@play-with-me/ui';
 import HomeIcon from '@mui/icons-material/Home';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import CloseIcon from '@mui/icons-material/Close';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import {
   Drawer,
   List,
@@ -11,24 +14,27 @@ import {
   ListItemIcon,
   DrawerProps,
   Divider,
+  Typography,
 } from '@mui/material';
 import Welcome from '../../../assets/welcome.png';
+import { Box } from '@mui/system';
+import { MouseEventHandler } from 'react';
 
 interface NavLink {
   text: string;
   link?: string;
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
 }
 const NAVLINKS: NavLink[] = [
   { text: 'דף בית', icon: <HomeIcon /> },
   { text: 'מגרשים', icon: <SportsSoccerIcon /> },
-  { text: 'שחקנים מובילם', icon: <EmojiEventsIcon /> },
+  { text: 'שחקנים מובילים', icon: <EmojiEventsIcon /> },
 ];
 
 interface AppDrawerProps extends DrawerProps {}
 
 const AppDrawer = (props: AppDrawerProps) => {
-  const { toggleDarkMode } = useDarkMode();
+  const { darkMode, theme, toggleDarkMode } = useDarkMode();
 
   return (
     <Drawer
@@ -36,26 +42,50 @@ const AppDrawer = (props: AppDrawerProps) => {
       anchor="right"
       sx={{ '& .MuiDrawer-paper': { left: 0, right: 'auto' } }}
     >
-      <div style={{ height: '100%', width: '500px' }}>
+      <div style={{ height: '100%', width: '500px', maxWidth: '95vw' }}>
         <div
           style={{
             height: '27%',
-            marginTop: 20,
+            marginTop: 15,
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'space-between',
             alignItems: 'center',
           }}
         >
+          <Box
+            style={{
+              cursor: 'pointer',
+              position: 'absolute',
+              right: '3%',
+            }}
+            sx={{
+              '&:hover': {
+                color: `${theme.palette.primary.main}`,
+              },
+            }}
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </Box>
+          <span onClick={props.onClose as MouseEventHandler}>
+            <CloseIcon
+              sx={{
+                '&:hover': {
+                  color: `${theme.palette.error.main}`,
+                },
+              }}
+              style={{ cursor: 'pointer', position: 'absolute', left: '3%' }}
+            />
+          </span>
           <img
             src={Welcome}
-            alt="Example"
-            style={{ width: '330px', height: '215px' }}
+            alt="welcome"
+            style={{ width: '70%', height: '80%' }}
           />
           <Button
             sx={{ height: '17%', fontWeight: 'bold' }}
             variant="contained"
-            onClick={toggleDarkMode}
           >
             התברות / הרשמה
           </Button>
@@ -75,7 +105,20 @@ const AppDrawer = (props: AppDrawerProps) => {
           </List>
         </div>
         <Divider sx={{ mt: 3, borderBottomWidth: 2 }} />
-        <div style={{ height: '7%' }}></div>
+        <div
+          style={{
+            height: '7%',
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="subtitle1">מי אנחנו</Typography>
+          <Typography variant="subtitle1">|</Typography>
+          <Typography variant="subtitle1">שאלות נפוצות</Typography>
+          <Typography variant="subtitle1">|</Typography>
+          <Typography variant="subtitle1">צרו קשר</Typography>
+        </div>
       </div>
     </Drawer>
   );
--- libs/ui/src/providers/DarkMode.tsx
+++ libs/ui/src/providers/DarkMode.tsx
@@ -49,6 +49,11 @@ const getTheme = (isDarkMode: boolean) => {
         secondary: isDarkMode ? '#B0B0B0' : '#666666',
       },
     },
+    typography: {
+      subtitle1: {
+        color: '#808080',
+      },
+    },
   });
 };
 
